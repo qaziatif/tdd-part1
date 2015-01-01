fc2aed533b63c8127103eca5f64457c7a25f1657
Qazi Atif <Qazi Atif@QaziAtif-PC>
2015-01-01 12:45:50 +0500

    Chapter 13

--- src/Bank.java
+++ src/Bank.java
@@ -1,6 +1,24 @@
 
 public class Bank {
+	
+	/*Money reduce(Expression source, String to) {
+		Sum sum= (Sum) source;
+		int amount= sum.augend.amount + sum.addend.amount;
+		return new Money(amount, to);
+	}*/
+	/*Money reduce(Expression source, String to) {
+		Sum sum= (Sum) source;
+		return sum.reduce(to);
+	}*/
+	
+/*	Money reduce(Expression source, String to) {
+		if (source instanceof Money)
+			return (Money) source.reduce(to);
+		Sum sum= (Sum) source;
+		return sum.reduce(to);
+	}*/
+	
 	Money reduce(Expression source, String to) {
-		return Money.dollar(10);
+		return source.reduce(to);
 	}
 }
--- src/DollarTest.java
+++ src/DollarTest.java
@@ -67,4 +67,25 @@ public class DollarTest {
 		Money reduced= bank.reduce(sum, "USD");
 		assertEquals(Money.dollar(10), reduced);
 	}
+	
+	public void testPlusReturnsSum() {
+		Money five= Money.dollar(5);
+		Expression result= five.plus(five);
+		Sum sum= (Sum) result;
+		assertEquals(five, sum.augend);
+		assertEquals(five, sum.addend);
+	}
+	
+	public void testReduceSum() {
+		Expression sum= new Sum(Money.dollar(3), Money.dollar(4));
+		Bank bank= new Bank();
+		Money result= bank.reduce(sum, "USD");
+		assertEquals(Money.dollar(7), result);
+	}
+	
+	public void testReduceMoney() {
+		Bank bank= new Bank();
+		Money result= bank.reduce(Money.dollar(1), "USD");
+		assertEquals(Money.dollar(1), result);
+	}
 }
